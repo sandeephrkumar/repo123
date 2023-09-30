b0db50e5a20b1d1f51945dcf9e66d38acde3f619
sandeephrkumar <sandeephrkumar@gmail.com>
2023-09-30 14:06:54 +0000

    added IST time to function

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-from datetime import datetime
+from datetime import datetime,  timedelta
+
 
 now = datetime.now()
 
 
 current_time = now.strftime("%H:%M:%S")
 print("Current Time =", current_time)
+result = now + timedelta(minutes=330) 
+print("India Current Time =", result)
